70354c6af29661a75ac008f9192ae7ef8876bbaf
Xenoleth <petar.yerzhabek@abv.bg>
2017-11-24 14:11:17 +0200

    Create auth server, resource server and client

---
+++ AuthorizationServer/ResourceServer/Controllers/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ResourceServer.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+            return new JsonResult(User.Claims.Select(c => new { c.Type, c.Value }));
+        }
+    }
+}
